70a295a0beff3dedd135eb298aa97af99bd0f6e2
Nicholas Sturgeon <sturgeon.nick@icloud.com>
2021-12-15 01:14:36 +0000

    Add background transition

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,23 @@ import { computed } from "vue";
 import { useRoute } from "vue-router";
 import NavMenu from "@/components/common/Menu.vue";
 import NavFooter from "@/components/common/Footer.vue";
+
 const route = useRoute();
 const bg = computed(() => route.params.slug);
 </script>
 
 <template>
-  <div
-    class="min-w-screen flex flex-col min-h-screen bg-zelda bg-left bg-cover bg-fixed"
-    :class="bg === 'oot' ? 'bg-oot' : bg === 'mm' ? 'bg-mm' : ''"
-  >
+  <transition name="fade">
+    <div
+      v-if="bg === 'oot'"
+      class="bg-oot bg-left top-0 bottom-0 right-0 bg-cover fixed w-full"
+    ></div>
+    <div
+      v-else-if="bg === 'mm'"
+      class="bg-mm bg-left top-0 bottom-0 right-0 bg-cover fixed w-full"
+    ></div>
+  </transition>
+  <div class="flex flex-col min-h-screen relative">
     <nav-menu />
     <div class="container flex-grow m-auto md:px-8">
       <router-view />
@@ -19,3 +27,15 @@ const bg = computed(() => route.params.slug);
     <nav-footer />
   </div>
 </template>
+
+<style>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
