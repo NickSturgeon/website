aff198321a853f171b0a580814528b616afee46b
Nicholas Sturgeon <sturgeon.nick@icloud.com>
2021-12-15 01:57:30 +0000

    Attempt to smooth out transition

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,24 @@
 <script setup lang="ts">
-import { computed } from "vue";
+import { computed, onMounted } from "vue";
 import { useRoute } from "vue-router";
 import NavMenu from "@/components/common/Menu.vue";
 import NavFooter from "@/components/common/Footer.vue";
+
+// Preload bg images for smooth transitions
+onMounted(() => {
+  const bg = [
+    new URL("./assets/img/bg-oot.png", import.meta.url).href,
+    new URL("./assets/img/bg-mm.png", import.meta.url).href,
+  ];
+  bg.forEach((bg) => (new Image().src = bg));
+});
 
 const route = useRoute();
 const bg = computed(() => route.params.slug);
 </script>
 
 <template>
-  <transition name="fade">
+  <transition appear name="fade">
     <div
       v-if="bg === 'oot'"
       class="bg-oot bg-left top-0 bottom-0 right-0 bg-cover fixed w-full"
@@ -17,6 +26,10 @@ const bg = computed(() => route.params.slug);
     <div
       v-else-if="bg === 'mm'"
       class="bg-mm bg-left top-0 bottom-0 right-0 bg-cover fixed w-full"
+    ></div>
+    <div
+      v-else
+      class="bg-purple bg-left top-0 bottom-0 right-0 bg-cover fixed w-full"
     ></div>
   </transition>
   <div class="flex flex-col min-h-screen relative">
