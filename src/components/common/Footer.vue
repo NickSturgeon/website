5680c5dbca08ef02449c0a89fe58bbe104be9f38
Nicholas Sturgeon <sturgeon.nick@icloud.com>
2021-12-17 23:23:09 +0000

    Update footer

--- src/components/common/Footer.vue
+++ src/components/common/Footer.vue
@@ -3,10 +3,17 @@ const currentYear = new Date().getFullYear();
 </script>
 
 <template>
-  <div class="text-white text-center w-screen h-14 bg-black bg-opacity-20 mt-4">
-    <p class="text-lg mt-4">
-      &copy; Zelda RET 2020-{{ currentYear }}. To contact us, please
-      <a class="text-gold" href="#">join our Discord server</a>
-    </p>
+  <div
+    class="text-white flex justify-center items-center w-screen h-14 bg-black bg-opacity-20 mt-4"
+  >
+    <p class="text-lg mx-2">&copy; Zelda RET 2020-{{ currentYear }}</p>
+    <div class="flex mx-2">
+      <a href="https://discord.zelda64.dev" target="_blank" title="Discord">
+        <img class="h-6 mx-2" src="../../assets/img/icons/discord.png" />
+      </a>
+      <a href="https://github.com/zeldaret" target="_blank" title="Github">
+        <img class="h-6 mx-2" src="../../assets/img/icons/github.png" />
+      </a>
+    </div>
   </div>
 </template>
