ccda592688a411e89afed92bb47ab603c2b1ce05
Nicholas Sturgeon <sturgeon.nick@icloud.com>
2021-12-17 22:43:00 +0000

    Improve menu button

--- src/components/common/Menu.vue
+++ src/components/common/Menu.vue
@@ -1,12 +1,8 @@
 <script setup lang="ts">
-import { ref } from "vue";
+import { Menu as MenuDropdown, MenuButton, MenuItems, MenuItem } from "@headlessui/vue";
+import { ChevronDownIcon } from "@heroicons/vue/solid";
 
 import games from "../../assets/json/games.json";
-
-const menuOpen = ref(false);
-
-const toggleMenu = () => (menuOpen.value = !menuOpen.value);
-const closeMenu = () => (menuOpen.value = false);
 </script>
 
 <template>
@@ -21,35 +17,43 @@ const closeMenu = () => (menuOpen.value = false);
           />
           <span class="text-white align-middle text-2xl font-bold mr-12">Zelda RET</span>
         </router-link>
-        <div class="dropdown inline-block relative h-14">
-          <span
-            class="text-white inline-block align-sub text-lg cursor-pointer h-10"
-            @click="toggleMenu"
+        <menu-dropdown as="div" class="relative inline-block">
+          <menu-button
+            class="inline-flex items-center justify-center w-full text-white align-sub text-lg bg-white bg-opacity-10 hover:bg-opacity-20 rounded-md px-4"
           >
             Games
-          </span>
-          <div
-            class="text-black menu absolute px-4 py-2 z-50 text-center -right-16 w-48 bg-white rounded-md"
-            :class="menuOpen ? 'block' : 'hidden'"
+            <ChevronDownIcon
+              class="w-5 h-5 ml-2 -mr-1 mt-0.5 text-white"
+              aria-hidden="true"
+            />
+          </menu-button>
+          <transition
+            enter-active-class="transition duration-100 ease-out"
+            enter-from-class="transform scale-95 opacity-0"
+            enter-to-class="transform scale-100 opacity-100"
+            leave-active-class="transition duration-75 ease-in"
+            leave-from-class="transform scale-100 opacity-100"
+            leave-to-class="transform scale-95 opacity-0"
           >
-            <template v-for="game in games">
-              <router-link
-                :to="`/games/${game.slug}`"
-                class="nav-link"
-                @click="closeMenu"
-              >
-                <p class="font-semibold py-1">{{ game.title }}</p>
-              </router-link>
-            </template>
-          </div>
-        </div>
+            <menu-items
+              class="text-black p-1 z-50 text-left w-48 bg-white rounded-md absolute right-0"
+            >
+              <template v-for="game in games">
+                <menu-item v-slot="{ active }">
+                  <router-link :to="`/games/${game.slug}`" class="nav-link">
+                    <p
+                      class="font-semibold p-2"
+                      :class="active ? 'bg-purple text-white rounded-md' : ''"
+                    >
+                      {{ game.title }}
+                    </p>
+                  </router-link>
+                </menu-item>
+              </template>
+            </menu-items>
+          </transition>
+        </menu-dropdown>
       </div>
     </div>
   </div>
 </template>
-
-<style>
-.dropdown:hover .menu {
-  display: block;
-}
-</style>
