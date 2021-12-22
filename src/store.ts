4db21897a1c44fac2126ff72a296f99459c955ff
Nicholas Sturgeon <sturgeon.nick@icloud.com>
2021-12-22 06:21:36 +0000

    Use constants for vuex functions

--- src/store.ts
+++ src/store.ts
@@ -13,29 +13,38 @@ export interface State {
 
 const cacheTimeoutMs: number = 5 * 60 * 1000; // 5m
 
+export const GET_SET_CSV_CACHE = "getSetCsvCache";
+
+const ADD_CSV_CACHE = "addCsvCache",
+  REMOVE_CSV_CACHE = "removeCsvCache";
+
 export const store = createStore<State>({
   state: {
     csvCache: [],
   },
   mutations: {
-    addCsvCache(state, { key, data }) {
+    [ADD_CSV_CACHE](state, { key, data }) {
       state.csvCache.push({ key, data });
     },
-    removeCsvCache(state, key) {
+    [REMOVE_CSV_CACHE](state, key) {
       const index = state.csvCache.findIndex((c) => c.key === key);
       state.csvCache.splice(index, 1);
     },
   },
   actions: {
-    getSetCsvCache: async (context, key) => {
-      const busted = undefined;
-      const cache = context.state.csvCache.find((c) => c.key === key);
+    [GET_SET_CSV_CACHE]: async (context, key) => {
+      let cache = context.state.csvCache.find((c) => c.key === key);
       let data = cache?.data;
 
-      if (cache === busted) {
+      if (cache === undefined) {
         data = await fetch(`${constants.resourceURL}/csv/${key}.csv`).then((res) => res.text());
-        context.commit("addCsvCache", { key, data });
-        setTimeout(() => context.commit("removeCsvCache", key), cacheTimeoutMs);
+        cache = context.state.csvCache.find((c) => c.key === key);
+        if (cache === undefined) {
+          context.commit(ADD_CSV_CACHE, { key, data });
+          setTimeout(() => context.commit(REMOVE_CSV_CACHE, key), cacheTimeoutMs);
+        } else {
+          data = cache.data;
+        }
       }
 
       return data!;
--- src/utilities/helpers.ts
+++ src/utilities/helpers.ts
@@ -1,5 +1,5 @@
 import { Store } from "vuex";
-import { State } from "../store";
+import { State, GET_SET_CSV_CACHE } from "../store";
 
 export async function scrollToHash(hash: string) {
   const checkDelayMs = 500;
@@ -18,7 +18,7 @@ export async function scrollToHash(hash: string) {
 
 export async function cacheCSV(store: Store<State>, keys: string[]): Promise<string[]> {
   const results: Promise<string>[] = [];
-  keys.forEach((key) => results.push(store.dispatch("getSetCsvCache", key)));
+  keys.forEach((key) => results.push(store.dispatch(GET_SET_CSV_CACHE, key)));
   return Promise.all(results);
 }
 
