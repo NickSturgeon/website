820b2ceaf798a51aee5ef8d38a808927439f279a
Nicholas Sturgeon <sturgeon.nick@icloud.com>
2021-12-24 04:20:21 +0000

    Fix up statement

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,18 @@
 <script setup lang="ts">
 import { ref } from "vue";
-import { Dialog as StatementDialogue, DialogOverlay, DialogTitle } from "@headlessui/vue";
+import {
+  Dialog as StatementDialogue,
+  DialogOverlay,
+  DialogTitle,
+  TransitionRoot,
+  TransitionChild,
+} from "@headlessui/vue";
 import { XIcon } from "@heroicons/vue/outline";
 import games from "../assets/json/games.json";
 import md from "../assets/statement.md";
 
 const statement = ref();
-const viewStatement = ref(false);
+const showStatement = ref(false);
 </script>
 
 <template>
@@ -15,7 +21,7 @@ const viewStatement = ref(false);
     <div class="flex items-center justify-center">
       <button
         type="button"
-        @click="viewStatement = true"
+        @click="showStatement = true"
         class="px-4 py-2 text-sm font-medium text-white bg-black rounded-md bg-opacity-20 hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
       >
         View Statement
@@ -29,30 +35,51 @@ const viewStatement = ref(false);
     </div>
   </div>
 
-  <statement-dialogue
-    :open="viewStatement"
-    :initial-focus="null"
-    @close="viewStatement = false"
-    class="fixed inset-x-0 inset-y-20 shadow-md w-full max-w-xl mx-auto z-10 overflow-y-auto p-8 bg-white rounded-md"
-  >
-    <teleport :to="'body'">
-      <dialog-overlay class="fixed transform inset-0 bg-black opacity-50" />
-    </teleport>
+  <transition-root appear as="template" :show="showStatement">
+    <statement-dialogue
+      as="div"
+      @close="showStatement = false"
+      class="fixed inset-0 z-10 overflow-y-auto"
+    >
+      <div class="flex items-center justify-center min-h-screen">
+        <transition-child
+          enter="duration-300 ease-out"
+          enter-from="opacity-0"
+          enter-to="opacity-100"
+          leave="duration-200 ease-in"
+          leave-from="opacity-100"
+          leave-to="opacity-0"
+        >
+          <dialog-overlay class="fixed inset-0 bg-black opacity-30 rounded" />
+        </transition-child>
 
-    <div class="flex justify-between">
-      <dialog-title as="h2" class="text-xl font-bold leading-6 text-gray-900">
-        {{ statement?.frontmatter?.title ?? "Statement" }}
-      </dialog-title>
-      <button @click="viewStatement = false">
-        <x-icon class="w-6 h-6 text-red-500" />
-      </button>
-    </div>
+        <transition-child
+          enter="duration-300 ease-out"
+          enter-from="opacity-0 scale-95"
+          enter-to="opacity-100 scale-100"
+          leave="duration-200 ease-in"
+          leave-from="opacity-100 scale-100"
+          leave-to="opacity-0 scale-95"
+        >
+          <div class="relative w-full max-w-2xl p-6 bg-white rounded">
+            <div class="flex justify-between">
+              <dialog-title as="h2" class="text-xl font-bold leading-6 text-gray-900">
+                {{ statement?.frontmatter?.title ?? "Statement" }}
+              </dialog-title>
+              <button @click="showStatement = false">
+                <x-icon class="w-6 h-6 text-red-500" />
+              </button>
+            </div>
 
-    <div class="mt-2">
-      <md class="markdown text-black" ref="statement" />
-      <p class="text-right mt-4">
-        — {{ statement?.frontmatter?.signature ?? "ZeldaRET" }}
-      </p>
-    </div>
-  </statement-dialogue>
+            <div class="mt-2">
+              <md class="markdown text-black" ref="statement" />
+              <p class="text-right mt-4">
+                — {{ statement?.frontmatter?.signature ?? "ZeldaRET" }}
+              </p>
+            </div>
+          </div>
+        </transition-child>
+      </div>
+    </statement-dialogue>
+  </transition-root>
 </template>
